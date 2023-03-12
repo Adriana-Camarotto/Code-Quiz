7127e7aecb78615e64fb02a3abb2132f8a80d32e
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-03-12 20:21:04 +0000

    transferred questions code to questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,49 +11,7 @@ let timeLeft,
     questionIndex, 
     score;
 
-const questions = [
-    {
-        question: `What is JavaScript?:`,
-        choices: [
-            "A markup language",
-            "A programming language",
-            "A stalyng language",
-            "A database language"
-        ],
-        answer: 1,
-    },
-    {
-        question: `Wich of the following is NOT a primitive data type in JavaScript?`,
-        choices: [
-            "Number",
-            "String",
-            "Boolean",
-            "Object"
-        ],
-        answer: 3,
-    },
-    {
-        question: `What is the purpose of a function in JavaScript?`,
-        choices: [ 
-            "To store data",
-            "To creat objects",
-            "To perform action and return values",
-            "To style elements on webpage"
-        ],
-        answer: 2,
-    },
-    {
-        question: `What is an array in JavaScript?`,
-        choices: [ 
-            "A type of objects stores a collection o values",
-            "A function that performs a specific task",
-            "A conditional statement that executes code if a certain condition is true",
-            "A loop that iterates over a colletion of values"
-        ],
-        answer: 0,
-    },
 
-];
 
 // Add event listener to the start button.
 startBtn.addEventListener(`click`, startQuiz);
